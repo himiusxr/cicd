eb1dcfcd7bf498c8a2db1154bb1d9bc9eb41a7dc
Himanshu <himanshu.44144@gmail.com>
2022-03-13 07:02:24 +0530

    Initial commit

---
+++ src/main/java/com/pipeline/cicd/WebappMavenDeployApplication.java
@@ -0,0 +1,13 @@
+package com.pipeline.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappMavenDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappMavenDeployApplication.class, args);
+	}
+
+}
